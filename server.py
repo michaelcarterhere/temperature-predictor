38d9abb098d2dd618db31ea648a52411b32989c0
michaelcarterhere <129359269+michaelcarterhere@users.noreply.github.com>
2024-12-27 20:57:26 -0500

    Update server.py

--- server.py
+++ server.py
@@ -58,4 +58,6 @@ def predict():
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Use the PORT environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
